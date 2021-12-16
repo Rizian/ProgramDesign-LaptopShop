d58c87a36ea0ce6a74009ad41dd6294cbdff8a9f
Rizian <ianwirawan81@gmail.com>
2021-12-17 04:34:41 +0700

    Initial Commit

---
+++ LaptopClass.py
@@ -0,0 +1,34 @@
+class Laptop:
+    "This is a class for a laptop brand"
+
+    def __init__(self, brand, price, stock):
+        self.set_brand(brand)
+        self.set_price(price)
+        self.set_stock(stock)
+
+    def __str__(self):
+        return f"Brand Name: {self.get_brand}\nPrice: {self.get_price:.2f}\nAvailable Stock: {self.get_stock}"
+
+    def set_brand(self, brand) -> str:
+        self.__brand = brand
+    def set_price(self, price) -> float:
+        self.__price = price
+    def set_stock(self, stock) -> int:
+        self.__stock = stock
+
+    def get_brand(self):
+        return self.__brand
+    def get_price(self):
+        return self.__price
+    def get_stock(self):
+        if self.__stock <= 0:
+            return "N/A"
+        return self.__stock
+        
+    def calc_total_purchase(self, amount):
+        total_cost = self.__price * amount
+        if amount > self.__stock:
+            return "Insufficient Stock"
+        else:
+            self.__stock -= amount
+        return total_cost
---
+++ LaptopShop.py
@@ -0,0 +1,85 @@
+from LaptopClass import Laptop
+
+# add more and change if needed | format: "name (all lowercase)" : Laptop("Name", price in dollars, amount in stock),
+laptops = {
+    "dell" : Laptop("Dell", 500, 12), 
+    "lenovo" : Laptop("Lenovo", 1300, 5),   # temporary placeholders, change if needed
+    "hp" : Laptop("HP", 850, 20),
+        }
+    
+# Displays the list of items available to purchase
+def printShopItems():
+    print("\t|---------------------------------------|")
+    print("\t|------------Laptops for sale-----------|")
+    print("\t|---------------------------------------|")
+    print("\t|Brand\t\t|Price\t\t|Stock\t|")
+    print("\t|---------------|---------------|-------|")
+    for i in laptops:
+        print(f"\t|{laptops[i].get_brand()}\t\t|${laptops[i].get_price():.2f}\t|{laptops[i].get_stock()}\t|")
+    print("\t|_______________|_______________|_______|")
+    print("")
+
+# Prints the selected purchase order
+def printOrder(item, amount, total):
+    print("\t|-----------------------------------------------|")
+    print("\t|-------------------Your Order------------------|")
+    print("\t|-----------------------------------------------|")
+    print("\t|Brand\t\t|Price\t\t|Amount\t\t|")
+    print("\t|---------------|---------------|---------------|")
+    print(f"\t|{laptops[item].get_brand()}\t\t|${laptops[item].get_price():.2f}\t|x{amount}\t\t|")
+    print("\t|-----------------------------------------------|")
+    print(f"\t|\t\tTotal Price:\t ${total:.2f}\t|")
+    print("\t|_______________________________________________|")
+
+def main():
+    while True:
+        printShopItems()
+        
+        while True:
+            select = input("Enter which laptop brand you would like to purchase >> ").lower()
+            if select not in laptops:
+                print("Brand not found. Please Re-enter.\n")
+            elif select in laptops and (laptops[select].get_stock() == "N/A"):
+                print(f"{laptops[select].get_brand()} laptops are currently out of stock.\n")
+                retry = input("Would you like to purchase a different brand? (Y/N) >> ").lower()
+                if retry == "y":
+                    continue
+                else:
+                    quit()
+            else:
+                break
+        
+        print(f"\nYou have chosen to purchase {laptops[select].get_brand()} laptop(s).\n")
+
+        while True:
+            try:
+                amount = int(input("Enter the amount you would like to purchase >> "))
+            except ValueError:
+                print("Please enter a number.\n")
+                continue
+            total = laptops[select].calc_total_purchase(amount)
+            if amount <= 0:
+                print("Purchases cannot be zero or negative.")
+                print("Please enter a positive number.\n")
+            elif total == "Insufficient Stock":
+                print("Not enough in stock for purchase. Please purchase within stock.\n")
+                continue
+            else:
+                break
+        
+        print(f"\nYou have chosen to purchase {amount} laptop(s).\n")
+        print("Your order:\n")
+        printOrder(select, amount, total)
+        print("")
+        
+        restart = input("Would you like to purchase more? (Y/N) >> ").lower()
+        if restart == "y":
+            print("Remaining Stock:\n")
+            continue
+        else:
+            print("Remaining Stock:\n")
+            printShopItems()
+            break
+
+if __name__ == "__main__":
+    main()
